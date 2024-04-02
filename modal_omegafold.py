fe3c62cbd96d5aff3d6ff9c091dbd7db5975601a
hgbrian <briannaughton@gmail.com>
2024-04-02 16:44:52 -0700

    gpu global

--- modal_omegafold.py
+++ modal_omegafold.py
@@ -1,14 +1,21 @@
+"""Run OmegaFold on a fasta file.
+
+If it runs out of memory, try changing GPU to "80GB".
+"""
+
 import glob
 
 from subprocess import run
 from pathlib import Path
 
+import modal
 from modal import Image, Mount, Stub
 
 FORCE_BUILD = False
 LOCAL_IN = "./in/omegafold"
 LOCAL_OUT = "./out/omegafold"
 REMOTE_IN = "/in"
+GPU = modal.gpu.A100(size="40GB")
 
 stub = Stub()
 
@@ -18,7 +25,7 @@ image = (Image
          .pip_install("git+https://github.com/HeliXonProtein/OmegaFold.git", force_build=FORCE_BUILD)
         )
 
-@stub.function(image=image, gpu="a100", timeout=60*15,
+@stub.function(image=image, gpu=GPU, timeout=60*15,
                mounts=[Mount.from_local_dir(LOCAL_IN, remote_path=REMOTE_IN)])
 def omegafold(input_fasta:str, subbatch_size:int) -> list[str, str]:
     input_fasta = Path(input_fasta).relative_to(LOCAL_IN)
@@ -26,8 +33,12 @@ def omegafold(input_fasta:str, subbatch_size:int) -> list[str, str]:
 
     Path(LOCAL_OUT).mkdir(parents=True, exist_ok=True)
 
-    run(["omegafold", "--model", "2", "--subbatch_size", str(subbatch_size),
-         Path(REMOTE_IN) / input_fasta.name, LOCAL_OUT], check=True)
+    run(["omegafold",
+         "--model", "2",
+         "--subbatch_size", str(subbatch_size),
+         Path(REMOTE_IN) / input_fasta.name,
+         LOCAL_OUT
+         ], check=True)
 
     return [(pdb_file, open(pdb_file, "rb").read())
             for pdb_file in glob.glob(f"{LOCAL_OUT}/**/*.pdb", recursive=True)]
