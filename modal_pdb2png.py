b5c00abf9178037f8aa8440499f4b9cd8015d653
hgbrian <briannaughton@gmail.com>
2024-06-21 07:57:54 -0700

    update pdb2png

--- modal_pdb2png.py
+++ modal_pdb2png.py
@@ -4,28 +4,28 @@ Visualize a pdb file as a png.
 TODO: center on the ligand, if there is one, and find the best orientation.
 """
 
+import json
 from pathlib import Path
+from typing import Union
 
-from modal import Image, Mount, Stub
+from modal import App, Image, Mount
 
 FORCE_BUILD = False
 LOCAL_IN = "./in/pdb2png"
 LOCAL_OUT = "./out/pdb2png"
 REMOTE_IN = "/in"
 
-stub = Stub()
+app = App()
 
-image = (Image
-         .micromamba(python_version = "3.11")
-         .micromamba_install("pymol-open-source==2.5.0", channels=["conda-forge"])
-         .apt_install("libgl1")
-         .apt_install("g++")
-         .pip_install(["ProDy==2.4.1"])
-        )
+image = (
+    Image.micromamba(python_version="3.11")
+    .micromamba_install("pymol-open-source==2.5.0", channels=["conda-forge"])
+    .apt_install("libgl1")
+    .apt_install("g++")
+    .pip_install(["ProDy==2.4.1"])
+)
 
 
-import json
-from typing import Union
 
 RENDER_OPTIONS = {
     "default": {
@@ -89,7 +89,7 @@ DEFAULT_PROTEIN_COLORS = (0.8, 0.8, 0.6, 0.8, 0.6, 0.8, 0.6, 0.8, 0.8)
 DEFAULT_HETATM_COLORS = (0.15, 0.7, 0.9, 0.9, 0.75, 0.15, 0.9, 0.15, 0.75)
 
 
-def apply_render_style(render_style:str) -> None:
+def apply_render_style(render_style: str) -> None:
     """Apply render styles from a dict.
     Everything is global, because pymol.
 
@@ -112,14 +112,22 @@ def apply_render_style(render_style:str) -> None:
             cmd.set(k, v)
 
 
-def get_orientation_for_ligand(pdb_file: str, ligand_id_or_chain: Union[str, tuple[str, str]]) -> tuple[list, float]:
-    from prody import parsePDB, calcCenter
+def get_orientation_for_ligand(
+    pdb_file: str, ligand_id_or_chain: Union[str, tuple[str, str]]
+) -> tuple[list, float]:
     import numpy as np
+    from prody import calcCenter, parsePDB
 
     structure = parsePDB(pdb_file)
     if isinstance(ligand_id_or_chain, tuple):
-        ligand_center = calcCenter(structure.select(f"resname {ligand_id_or_chain[0]} and chain {ligand_id_or_chain[1]}"))
-        nonligand_center = calcCenter(structure.select(f"not resname {ligand_id_or_chain[0]} and chain {ligand_id_or_chain[1]}"))
+        ligand_center = calcCenter(
+            structure.select(f"resname {ligand_id_or_chain[0]} and chain {ligand_id_or_chain[1]}")
+        )
+        nonligand_center = calcCenter(
+            structure.select(
+                f"not resname {ligand_id_or_chain[0]} and chain {ligand_id_or_chain[1]}"
+            )
+        )
     else:
         ligand_chain = structure.select(f"chain {ligand_id_or_chain}")
         ligand_resname = structure.select(f"resname {ligand_id_or_chain}")
@@ -139,24 +147,30 @@ def get_orientation_for_ligand(pdb_file: str, ligand_id_or_chain: Union[str, tup
     axis = np.cross(forward_vector, np.array([0, 0, 1]))
     angle = np.arccos(np.dot(forward_vector, np.array([0, 0, 1])))
 
-    return [float(v) for v in axis], float(angle * 180/np.pi)
+    return [float(v) for v in axis], float(angle * 180 / np.pi)
 
 
-@stub.function(image=image, gpu=None, timeout=60*15,
-               mounts=[Mount.from_local_dir(LOCAL_IN, remote_path=REMOTE_IN)])
-def pdb2png(pdb_file:str,
-            protein_rotate:Union[tuple[float, float, float], None]=None,
-            protein_color:Union[tuple[float, float, float], str, None]=None,
-            protein_zoom:Union[float, None]=None,
-            hetatm_color:Union[tuple[float, float, float], str, None]=None,
-            ligand_id:Union[str, None]=None,
-            ligand_chain:Union[str, None]=None,
-            ligand_zoom:float=None,
-            ligand_color:Union[tuple[float, float, float], str]="red",
-            show_water:bool=False,
-            render_style:str="default",
-            width:int=1600,
-            height:int=1600) -> list:
+@app.function(
+    image=image,
+    gpu=None,
+    timeout=60 * 15,
+    mounts=[Mount.from_local_dir(LOCAL_IN, remote_path=REMOTE_IN)],
+)
+def pdb2png(
+    pdb_file: str,
+    protein_rotate: Union[tuple[float, float, float], None] = None,
+    protein_color: Union[tuple[float, float, float], str, None] = None,
+    protein_zoom: Union[float, None] = None,
+    hetatm_color: Union[tuple[float, float, float], str, None] = None,
+    ligand_id: Union[str, None] = None,
+    ligand_chain: Union[str, None] = None,
+    ligand_zoom: float = None,
+    ligand_color: Union[tuple[float, float, float], str] = "red",
+    show_water: bool = False,
+    render_style: str = "default",
+    width: int = 1600,
+    height: int = 1600,
+) -> list:
     """
     Input is a pdb file.
     Output is a png file.
@@ -169,7 +183,7 @@ def pdb2png(pdb_file:str,
     cmd.reinitialize()
 
     cmd.load(in_pdb_file)
-    cmd.orient() # maybe not needed
+    cmd.orient()  # maybe not needed
 
     #
     # Rotation
@@ -179,17 +193,22 @@ def pdb2png(pdb_file:str,
         cmd.rotate("y", protein_rotate[1])
         cmd.rotate("z", protein_rotate[2])
     elif ligand_id is not None or ligand_chain is not None:
-        ligand_id_or_chain = (ligand_id, ligand_chain) if ligand_id and ligand_chain else (ligand_id or ligand_chain)
+        ligand_id_or_chain = (
+            (ligand_id, ligand_chain) if ligand_id and ligand_chain else (ligand_id or ligand_chain)
+        )
         _axis, _angle = get_orientation_for_ligand(str(in_pdb_file), ligand_id_or_chain)
         cmd.rotate(_axis, _angle)
 
+    #
+    # Colors
+    #
     if protein_color is None:
         protein_color = DEFAULT_PROTEIN_COLORS
 
     if isinstance(protein_color, tuple):
         n = 0
         for chain in cmd.get_chains():
-            cmd.set_color("protein_color", protein_color[n:n+3])
+            cmd.set_color("protein_color", protein_color[n : n + 3])
             cmd.color("protein_color", f"chain {chain} and not hetatm")
             n = (n + 3) % len(protein_color)
     else:
@@ -197,15 +216,17 @@ def pdb2png(pdb_file:str,
         cmd.color(protein_color, "not hetatm")
 
     # Color proteins and hetatms
-    for hp_id, hp_color, hp_sel in [("protein", protein_color, "not hetatm"),
-                                    ("hetatm", hetatm_color, "hetatm")]:
+    for hp_id, hp_color, hp_sel in [
+        ("protein", protein_color, "not hetatm"),
+        ("hetatm", hetatm_color, "hetatm"),
+    ]:
         if hp_color is not None:
             if isinstance(hp_color, tuple):
                 n = 0
                 for chain in cmd.get_chains():
                     cmd.select(f"sel_{hp_id}_{chain}", f"chain {chain} and {hp_sel}")
                     if cmd.count_atoms(f"sel_{hp_id}_{chain}") > 0:
-                        cmd.set_color(f"{hp_id}_color_{chain}", hp_color[n:n+3])
+                        cmd.set_color(f"{hp_id}_color_{chain}", hp_color[n : n + 3])
                         cmd.color(f"{hp_id}_color_{chain}", f"sel_{hp_id}_{chain}")
                         n = (n + 3) % len(hp_color)
             else:
@@ -234,6 +255,9 @@ def pdb2png(pdb_file:str,
         cmd.select("HOH", "resn HOH")
         cmd.hide("everything", "HOH")
 
+    #
+    # Render
+    #
     apply_render_style(render_style)
 
     cmd.ray(width, height)
@@ -244,21 +268,22 @@ def pdb2png(pdb_file:str,
     return [(out_png_path, open(out_png_path, "rb").read())]
 
 
-@stub.local_entrypoint()
-def main(input_pdb,
-         protein_rotate=None,
-         protein_color=None,
-         protein_zoom=None,
-         hetatm_color=None,
-         ligand_id=None,
-         ligand_chain=None,
-         ligand_zoom=None,
-         ligand_color="red",
-         show_water=False,
-         render_style="default",
-         width=1600,
-         height=1600) -> list:
-
+@app.local_entrypoint()
+def main(
+    input_pdb,
+    protein_rotate=None,
+    protein_color=None,
+    protein_zoom=None,
+    hetatm_color=None,
+    ligand_id=None,
+    ligand_chain=None,
+    ligand_zoom=None,
+    ligand_color="red",
+    show_water=False,
+    render_style="default",
+    width=1600,
+    height=1600,
+) -> list:
     if isinstance(protein_rotate, str):
         protein_rotate = tuple(map(float, protein_color.split(",")))
     if isinstance(protein_color, str) and "," in protein_color:
@@ -266,12 +291,24 @@ def main(input_pdb,
     if isinstance(ligand_color, str) and "," in ligand_color:
         ligand_color = tuple(map(float, ligand_color.split(",")))
 
-    outputs = pdb2png.remote(input_pdb, protein_rotate, protein_color, protein_zoom,
-                             hetatm_color, ligand_id, ligand_chain, ligand_zoom, ligand_color,
-                             show_water, render_style, width, height)
+    outputs = pdb2png.remote(
+        input_pdb,
+        protein_rotate,
+        protein_color,
+        protein_zoom,
+        hetatm_color,
+        ligand_id,
+        ligand_chain,
+        ligand_zoom,
+        ligand_color,
+        show_water,
+        render_style,
+        width,
+        height,
+    )
 
-    for (out_file, out_content) in outputs:
+    for out_file, out_content in outputs:
         Path(out_file).parent.mkdir(parents=True, exist_ok=True)
         if out_content:
-            with open(out_file, 'wb') as out:
+            with open(out_file, "wb") as out:
                 out.write(out_content)
